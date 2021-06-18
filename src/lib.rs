fec2fd8fa00b9bbf1eb100bfc69c6a1049f3b7df
Edyta Pawlak <edyta.pawlak@opensoftware.pl>
2021-06-18 18:15:52 +0200

    Add Testator struct
    Change-Id: Ia303ea9448d09df1e54b67e9653e0a34a0e1a37f

--- src/lib.rs
+++ src/lib.rs
@@ -16,3 +16,45 @@ use crate::{
     identifier::{BasicIdentifier, Identifier},
 };
 use std::str::FromStr;
+
+#[test]
+pub fn test_attestation_id_serialization() -> Result<(), Error> {
+    let testator_id = Identifier::Basic(BasicIdentifier {
+        id: "D5bw5KrpU2xRc3Oi4rsyBK9By6aotmXU0fNEybJbja1Q".into(),
+    });
+    let msg_str = "hi there";
+    let datum = Message {
+        message: msg_str.into(),
+    };
+    let ad = AttestationDatum::new(
+        AttestationId::new(testator_id, "123".into()),
+        Some(Identifier::Basic(BasicIdentifier { id: "123".into() })),
+        vec![],
+        Some(ObjectType::SAI("123".to_string())),
+        datum,
+        Some(ObjectType::SAI("4124".to_string())),
+    );
+    let id = ad.id;
+
+    let ser_id = serde_json::to_string(&id).unwrap();
+    assert_eq!(ser_id, "\"did:keri:D5bw5KrpU2xRc3Oi4rsyBK9By6aotmXU0fNEybJbja1Q/attestationId/jabUza-EpwNOQGALxFtFiMjC6PYdxlJqQtsI9E24uiI=\"");
+
+    let deser_id = AttestationId::from_str(&ser_id).unwrap();
+    assert_eq!(deser_id.testator_id.get_id(), id.testator_id.get_id());
+    assert_eq!(deser_id.id, id.id);
+
+    Ok(())
+}
+
+#[test]
+pub fn test_signed_datum_serialization() -> Result<(), Error> {
+    use crate::signed_attestation::SignedAttestationDatum;
+    let sd_str = r#"{"AttestationDatumId":"did:keri:DoQa-mkiBs5kDaSjbONUpryZKAJ4zGFn9EMHJPXykDA0/attestationId/vPjipY4kdlyt9e-p5SM7N_X6DQQD2VEuIfF9Wnrx3w4=","AttestedDatumSources":["did:keri:DoQa-mkiBs5kDaSjbONUpryZKAJ4zGFn9EMHJPXykDA0/attestationId/sourceID"],"Datum":{"issuer":"DoQa-mkiBs5kDaSjbONUpryZKAJ4zGFn9EMHJPXykDA0","message":"Some vc"},"proof":{"signature":"byfYjUug5s0fgwhQuzX4C03G6BwWYi7BMrd-ZoJC8AAuDEYg8duM1iNFn6_ZaTwlAW1QrMWbpGO9_hBvSAF4DQ=="}}"#;
+    let sd = SignedAttestationDatum::deserialize(sd_str)?;
+    assert_eq!(sd.serialize().unwrap(), sd_str);
+
+    Ok(())
+}
+fn create() {}
+
+fn verify() {}
